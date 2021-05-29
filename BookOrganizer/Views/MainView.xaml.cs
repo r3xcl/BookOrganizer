89255e7a6742ccf0f94ac48c6b88d5f14cd75718
r3xcl <pervih.matvey@gmail.com>
2021-05-29 15:27:25 +0300

    BookOrganizer

--- BookOrganizer/Views/MainView.xaml.cs
+++ BookOrganizer/Views/MainView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BookOrganizer
+namespace BookOrganizer.Views
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : Window
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
         }
