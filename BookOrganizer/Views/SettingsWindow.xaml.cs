1402cc457ee532246528962be5a932af2115e5d6
r3xcl <pervih.matvey@gmail.com>
2021-05-30 21:33:13 +0300

    Added animation of the settings window

--- BookOrganizer/Views/SettingsWindow.xaml.cs
+++ BookOrganizer/Views/SettingsWindow.xaml.cs
@@ -9,16 +9,20 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BookOrganizer.Views
 {
-    /// <summary>
-    /// Логика взаимодействия для SettingsWindow.xaml
-    /// </summary>
+
     public partial class SettingsWindow : Window
     {
+
+        private DispatcherTimer timer = new DispatcherTimer();
+        private int x;
+ 
         public SettingsWindow()
         {
             InitializeComponent();
@@ -26,12 +30,32 @@ namespace BookOrganizer.Views
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Timer();
         }
 
         private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        private void Timer()
+        {
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = 1.0,
+                To = 0.0,
+                Duration = new Duration(TimeSpan.FromSeconds(0.4))
+            };
+            this.BeginAnimation(Window.OpacityProperty, animation);
+
+            timer.Tick += new EventHandler(CloseWindow);
+            timer.Interval = new TimeSpan(0,0,0,0,400);
+            timer.Start();
+        }
+
+        private void CloseWindow(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
